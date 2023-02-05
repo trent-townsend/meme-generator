2e8b8854e292364ec23fac6fe8b32ea980904958
Trent Townsend <trent.townsend2@gmail.com>
2023-02-05 11:54:45 +1000

    Add random meme url state and change state on button press

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import memesData from '../../memesdata';
 
 export default function MemeForm() {
+    const memesArray = memesData.data.memes
+    const [randMeme, setRandMeme] = React.useState("")
+
+    function btnClick(e) {
+        setRandMeme(memesArray[Math.floor(Math.random() * memesArray.length)])
+        e.preventDefault();
+    }
+
     return (
         <form className='meme-form'>
             <span className='form-inputs'>
                 <input type="text" placeholder="Upper text" />
                 <input type="text" placeholder="Lower text" />
             </span>
-            <div className='meme-btn'>
-            <span>Get a new meme image</span>
-            </div>
+            <button className='meme-btn' onClick={btnClick}>Get a new meme image &#128444;</button>
+            <img className='meme-img' src={randMeme.url}/>
         </form>
     )
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 export default function NavBar() {
+
     return (
         <nav className="navbar">
             <span className="navbar-heading">
-            <img className="navbar-logo" src="../../public/troll-face.png" />
-            <h1>Meme Generator</h1>
+            <img className="navbar-logo" src="troll-face.png"  />
+            <h2>Meme Generator</h2>
             </span>
             <span><h2 className='course-name'>React Course - Project 3</h2></span>
         </nav>
